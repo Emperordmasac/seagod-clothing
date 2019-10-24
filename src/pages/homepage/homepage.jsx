f57e7ad7d54cf17582132a5f363fdc1c0a00ce7c
Emperordmasac <abiolawalter2015@gmail.com>
2019-10-24 14:02:42 +0100

    homepage divided into smaller components and finished styling the menu-item components

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Main from '../../components/main/main';
+
+import './homepage.scss';
+
+const HomePage = () => (
+    <div className ='homepage' >
+        <Main /> 
+    </div>
+
+);
+
+export default HomePage;
